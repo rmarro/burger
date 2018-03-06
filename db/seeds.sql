88b9e0e105247de8e0400bb6e4af4651804f6d11
rmarro <rmarro47@gmail.com>
2018-03-05 20:47:41 -0600

    set up initial files, made db and mysql connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
