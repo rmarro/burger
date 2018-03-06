26f6130952a6b14b78c5e3a1a08539b7deb5417b
rmarro <rmarro47@gmail.com>
2018-03-05 20:46:57 -0600

    set up file structure, created db and mysql connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    throw err
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
