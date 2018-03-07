9e7abe7a9f4039bb86983fd0147a4209472b28bc
rmarro <rmarro47@gmail.com>
2018-03-06 20:26:48 -0600

    router put working

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,6 +3,7 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
+// Get all burgers data
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var hbsObject = {
@@ -13,10 +14,24 @@ router.get("/", function(req, res) {
     });
 });
 
+// Make new burger
 router.post("/api/burgers", function(req, res) {
     burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
         // send back the id of the new burger
         res.json({id: result.insertId});
+    });
+});
+
+// Take in burger id and update its devoured status
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    var objColVals = "devoured = " + req.body.devoured;
+    burger.update(objColVals, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
